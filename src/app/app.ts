7bd5bb36f7f32b5ca23af1ce3a9b7ad27ddad130
Fio Ambrosetti <fio.ambrosetti@edeka.de>
2026-05-22 08:28:56 +0200

    unnötige Files gelöscht

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,59 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormBuilder, ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [ReactiveFormsModule],
+  // templateUrl: './app.html',
+  template: `<header>
+      <h1>EDeHS</h1>
+      <h2>Welcome, scatterbrain!</h2>
+      <h4>Let's do some goddamn shopping.</h4>
+    </header>
+    <main>
+      <section id="eingabe">
+        <h3>Neuen Artikel hinzufügen</h3>
+        <form [formGroup]="artikelForm" (ngSubmit)="addItem()">
+          <label for="artikel">Artikelbezeichnung:</label>
+          <input
+            formControlName="artikel"
+            type="text"
+            id="artikel"
+            placeholder="Brot, Eier, Milch..."
+          />
+          <label for="menge">Menge: </label>
+          <input formControlName="menge" type="number" id="menge" placeholder="42" />
+          <button [disabled]="!artikelForm.valid" type="submit">Hinzufügen!</button>
+        </form>
+      </section>
+      <section id="einkaufsliste">
+        <h3>Einkaufsliste</h3>
+        <ul>
+          @for (item of Liste; track $index) {
+            <li>{{ item }}</li>
+          }
+        </ul>
+      </section>
+    </main>`,
+  styleUrl: './app.css',
 })
 export class App {
+  Liste = [''];
   protected readonly title = signal('edhs');
+
+  public fb = inject(FormBuilder);
+
+  artikelForm = this.fb.nonNullable.group({
+    artikel: ['', Validators.required],
+    menge: [],
+  });
+
+  addItem() {
+    const test = this.artikelForm.getRawValue();
+    this.Liste.push(neuerArtikel.name);
+    console.log(this.Liste.length);
+  }
 }
